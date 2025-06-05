fe5424470b06e273c1edf762f568535150c31c7b
osama <osama.binkhunaizan.c@moneymoon.sa>
2025-06-05 19:21:36 +0300

    y

--- home.py
+++ home.py
@@ -108,12 +108,7 @@ def create_image_with_name(name, template_path="./001-Moneymoon-Eid-Greeting.jpg
     except IOError:
         font = ImageFont.load_default()  # Fallback, but won't support Arabic
 
-   # Get text size
-    bbox = draw.textbbox((0, 0), bidi_text, font=font)
-    text_width = bbox[2] - bbox[0]
-    text_height = bbox[3] - bbox[1]
-
-    # Get text size
+        # Get text size
     bbox = draw.textbbox((0, 0), bidi_text, font=font)
     text_width = bbox[2] - bbox[0]
     text_height = bbox[3] - bbox[1]
@@ -125,18 +120,22 @@ def create_image_with_name(name, template_path="./001-Moneymoon-Eid-Greeting.jpg
 
     # Draw text on the image
     draw.text((x, y), bidi_text, font=font, fill="#4DD6E9")
+#    # Get text size
+#     bbox = draw.textbbox((0, 0), bidi_text, font=font)
+#     text_width = bbox[2] - bbox[0]
+#     text_height = bbox[3] - bbox[1]
 
-    # # Get image size
-    # img_width, img_height = img.size
+#     # Get image size
+#     img_width, img_height = img.size
 
-    # # Center horizontally
-    # x = (img_width - text_width) / 2
+#     # Center horizontally
+#     x = (img_width - text_width) / 2
 
-    # # Move text up from the bottom (e.g. 150 pixels)
-    # y = img_height - text_height - 325
+#     # Move text up from the bottom (e.g. 150 pixels)
+#     y = img_height - text_height - 325
 
-    # # Draw text
-    # draw.text((x, y), bidi_text, font=font, fill="#4DD6E9")
+#     # Draw text
+#     draw.text((x, y), bidi_text, font=font, fill="#4DD6E9")
 
 
 

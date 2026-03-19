33a526f58165f2ae39ddae69413ebdcac35729e1
Osama <442102776@student.ksu.edu.sa>
2026-03-19 16:22:21 +0300

    Color update

--- home.py
+++ home.py
@@ -118,7 +118,7 @@ def create_image_with_name(name, template_path="./Eid-Greeting2.jpg"):
     y = (img_height - text_height) / 2
 
     # Draw text on the image
-    draw.text((x, y-100), bidi_text, font=font, fill="#008afe")
+    draw.text((x, y-100), bidi_text, font=font, fill="#43FFAE")
 
     return img
 
